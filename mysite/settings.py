259df03b4e17a91b0d9bd0d005bb367059a5a6ec
yari <yari.taft@gmail.com>
2019-11-09 02:52:20 -0300

    Debug default false added, don't know why it is not working the env variable in false

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = '8yrzcb+uvem9h8q8!s-*()*o7hz3d$qplj$=6gnoe-u57ka=b('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG_MODE")
+DEBUG = os.getenv("DEBUG_MODE",False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://afternoon-tundra-88183.herokuapp.com','afternoon-tundra-88183.herokuapp.com','yaritaft.com','https://yaritaft.com','www.yaritaft.com']
 
