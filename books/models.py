86d7354113cda3ad41dc8af9f8e08645321a4c86
yari <yari.taft@gmail.com>
2020-03-09 12:31:34 -0300

    Str method added to Books models.

--- books/models.py
+++ books/models.py
@@ -8,6 +8,9 @@ from .enums import EnumBookRates
 class BookTag(models.Model):
     keyword = models.CharField(null=False, unique=True, max_length=100)
 
+    def __str__(self):
+        return "%s" % (str(self.keyword),)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=300, null=False)
@@ -19,3 +22,6 @@ class Book(models.Model):
     link = models.CharField(max_length=700, null=False)
     author = models.CharField(max_length=200, null=False)
     edition = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return "%s - Author:  %s" % (str(self.title), str(self.author),)
