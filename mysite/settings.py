2284973d7b256b2f8e025ba484a3cc871a9a86b1
yari <yari.taft@gmail.com>
2020-03-15 03:10:59 -0300

    Minor fixes.

--- mysite/settings.py
+++ mysite/settings.py
@@ -171,3 +171,16 @@ STATICFILES_DIRS = [
         "common_resources",
     ),
 ]
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
+        },
+    },
+}
