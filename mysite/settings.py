eff7f4731b49e8aadf0933f44d43d1378ecc9e09
yari <yari.taft@gmail.com>
2020-03-15 02:54:42 -0300

    Minor fixes.

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = "mysite.urls"
 
