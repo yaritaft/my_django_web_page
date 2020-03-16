3986dc7f984bc244525ad4fc109d7c37c142410a
yari <yari.taft@gmail.com>
2020-03-16 01:24:04 -0300

    Run.

--- mysite/settings.py
+++ mysite/settings.py
@@ -56,6 +56,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
