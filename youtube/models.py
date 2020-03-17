3c4056917d6da516405f4e4fb94ed66cb945c65a
yari <yari.taft@gmail.com>
2020-03-17 15:34:04 -0300

    x

--- youtube/models.py
+++ youtube/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class YoutubeVideo(models.Model):
     title = models.CharField(max_length=300, null=False)
     video_youtube_id = models.CharField(max_length=700, null=False)
+    created_date = models.DateTimeField(null=False, blank=True)
 
     @property
     def youtube_url(self):
