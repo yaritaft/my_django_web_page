cc12b31e49b1207f31af24dcff5078b5791782da
yari <yari.taft@gmail.com>
2020-03-24 03:20:38 -0300

    Little changes to settings and dockerfile.

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "8yrzcb+uvem9h8q8!s-*()*o7hz3d$qplj$=6gnoe-u57ka=b("
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG_MODE") == "True"
